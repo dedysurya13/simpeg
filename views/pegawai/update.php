ce99b8a24fb84fc419f24585571f28319ad7f88f
Dedy Surya Prihantoro <dedy131098@gmail.com>
2020-09-03 11:53:48 +0700

    update tampilan update

--- views/pegawai/update.php
+++ views/pegawai/update.php
@@ -5,17 +5,18 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 /* @var $model app\models\Pegawai */
 
-$this->title = 'Update Pegawai: ' . $model->id;
-$this->params['breadcrumbs'][] = ['label' => 'Pegawais', 'url' => ['index']];
-$this->params['breadcrumbs'][] = ['label' => $model->id, 'url' => ['view', 'id' => $model->id]];
+$this->title = 'Update Pegawai: ' . $model->nama;
+$this->params['breadcrumbs'][] = ['label' => 'Pegawai', 'url' => ['index']];
+$this->params['breadcrumbs'][] = ['label' => $model->nama, 'url' => ['view', 'nama' => $model->nama]];
 $this->params['breadcrumbs'][] = 'Update';
 ?>
 <div class="pegawai-update">
-
-    <h1><?= Html::encode($this->title) ?></h1>
-
-    <?= $this->render('_form', [
-        'model' => $model,
-    ]) ?>
-
+    <div class="box box-success">
+        <div>
+            <h3 class="box-title"><?=Html::encode($this->title)?></h3>
+        </div>
+        <div class="box-body">
+            <?= $this->render('_form', ['model'=>$model])?>
+        </div>
+    </div>
 </div>
