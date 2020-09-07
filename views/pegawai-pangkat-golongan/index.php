dfcf664a97bb2cf320a46efd3177b50029030998
Dedy Surya Prihantoro <dedy131098@gmail.com>
2020-09-07 11:02:19 +0700

    Update index.php

--- views/pegawai-pangkat-golongan/index.php
+++ views/pegawai-pangkat-golongan/index.php
@@ -7,39 +7,33 @@ use yii\grid\GridView;
 /* @var $searchModel app\models\PegawaiPangkatGolonganSearch */
 /* @var $dataProvider yii\data\ActiveDataProvider */
 
-$this->title = 'Pegawai Pangkat Golongans';
+$this->title = 'History Pangkat/Golongan '.\app\models\Pegawai::findOne($id_pegawai)->nama;
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="pegawai-pangkat-golongan-index">
+    <div class="box">
+        <div class="box-header with-border">
+            <h3 class="box-title"><?= Html::encode($this->title) ?></h3>
+        </div>
+        <div class="box-body">
+            <?= GridView::widget([
+                'dataProvider' => $dataProvider,
+                'filterModel' => $searchModel,
+                'columns' => [
+                    ['class' => 'yii\grid\SerialColumn'],
+                    'masterPangkatGolongan.golongan',
+                    'masterPangkatGolongan.pangkat',
+                    'tanggal_sk',
+                    'no_sk',
+                    //'scan:ntext',
+                    //'created_by',
+                    //'created_date',
+                    //'updated_by',
+                    //'updated_date',
 
-    <h1><?= Html::encode($this->title) ?></h1>
-
-    <p>
-        <?= Html::a('Create Pegawai Pangkat Golongan', ['create'], ['class' => 'btn btn-success']) ?>
-    </p>
-
-    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
-
-    <?= GridView::widget([
-        'dataProvider' => $dataProvider,
-        'filterModel' => $searchModel,
-        'columns' => [
-            ['class' => 'yii\grid\SerialColumn'],
-
-            'id',
-            'id_master_pangkat_golongan',
-            'id_pegawai',
-            'tanggal_sk',
-            'no_sk',
-            //'scan:ntext',
-            //'created_by',
-            //'created_date',
-            //'updated_by',
-            //'updated_date',
-
-            ['class' => 'yii\grid\ActionColumn'],
-        ],
-    ]); ?>
-
-
+                    ['class' => 'yii\grid\ActionColumn'],
+                ],
+            ]); ?>
+        </div>
+    </div>
 </div>
