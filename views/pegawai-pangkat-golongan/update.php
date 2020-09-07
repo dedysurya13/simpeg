1b359febf19e84c316bdea21c17ca47ca2da56b5
Dedy Surya Prihantoro <dedy131098@gmail.com>
2020-09-07 11:02:25 +0700

    Update update.php

--- views/pegawai-pangkat-golongan/update.php
+++ views/pegawai-pangkat-golongan/update.php
@@ -5,18 +5,20 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 /* @var $model app\models\PegawaiPangkatGolongan */
 
-$this->title = 'Update Pegawai Pangkat Golongan: ' . $model->id;
-$this->params['breadcrumbs'][] = ['label' => 'Pegawai Pangkat Golongans', 'url' => ['index']];
-$this->params['breadcrumbs'][] = ['label' => $model->id, 'url' => ['view', 'id' => $model->id]];
+$this->title = 'Update SK '.$model->masterPangkatGolongan->golongan .' '. $model->pegawai->nama;
 $this->params['breadcrumbs'][] = 'Update';
 ?>
 <div class="pegawai-pangkat-golongan-update">
 
-    <h1><?= Html::encode($this->title) ?></h1>
-
-    <?= $this->render('_form', [
-          'model' => $model,
-          'pangkatGolonganArray' => $pangkatGolonganArray,
-     ]) ?>
-
+    <div class="box">
+    	<div class="box-header with-border">
+    		<h3 class="box-title"><?= Html::encode($this->title) ?></h3>
+    	</div>
+    	<div class="box-body">
+    		<?= $this->render('_form', [
+		        'model' => $model,
+		        'pangkatGolonganArray' => $pangkatGolonganArray,
+		    ]) ?>
+    	</div>
+    </div>
 </div>
