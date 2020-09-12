46c0f6a958dc12656588bb2ca571d498c1f4ee4b
Dedy Surya Prihantoro <dedy131098@gmail.com>
2020-09-12 11:27:51 +0700

    Update console.php

--- config/console.php
+++ config/console.php
@@ -27,14 +27,15 @@ $config = [
         ],
         'db' => $db,
     ],
+    
     'params' => $params,
-    /*
+    
     'controllerMap' => [
         'fixture' => [ // Fixture generation command line.
             'class' => 'yii\faker\FixtureController',
         ],
     ],
-    */
+    
 ];
 
 if (YII_ENV_DEV) {
